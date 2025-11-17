26024bd7ebc90267c770dd9395ba6592a9a8bfaa
Jeffrey Zhang <jeffreyzhang@mac.lan>
2025-11-17 11:33:35 -0500

    pre demo state

--- src/index.ts
+++ src/index.ts
@@ -6,74 +6,11 @@ const typeDefs = /* GraphQL */ `
     hello: String!
     project(id: Int!): Project!
   }
-
-  type Person {
-    id: Int!
-    name: String!
-  }
-  
-  type Project {
-    id: Int!
-    client: Person!
-    team: String!
-    members: [Person!]!
-  }
-
-  input PersonInput {
-    id: Int!
-    name: String!
-  }
-
-  type Mutation {
-    addProject(client: PersonInput!, team: String!, members: [PersonInput!]!): Project!
-  }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World!',
-    project: (parent: any, args: any) => {
-      return {
-        id: args.id,
-        client: {
-          id: 0,
-          name: 'Kris Jordan',
-        },
-        team: 'Team C',
-        members: [
-          {
-            id: 1,
-            name: 'Jeffrey Zhang',
-          },
-          {
-            id: 2,
-            name: 'Christian Lee',
-          },
-          {
-            id: 3,
-            name: 'Armaan Punj',
-          },
-          {
-            id: 4,
-            name: 'Kamal Vasireddy',
-          },
-        ],
-      };
-    },
-  },
-
-  Mutation: {
-    addProject: (parent: any, args: any) => {
-      return {
-        id: 2,
-        client: {
-          id: args.client.id,
-          name: args.client.name + ' *created in db*',
-        },
-        team: args.team,
-        members: args.members,
-      };
-    },
   },
 };
 
